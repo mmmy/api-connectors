d8571aea3f4c60f6705388a429732415cff6c655
yangqi <531905408@qq.com>
2018-07-31 23:33:16 +0800

    update real-account test\

--- official-ws/nodejs/strategy/test/real-account.js
+++ official-ws/nodejs/strategy/test/real-account.js
@@ -1,9 +1,14 @@
 
 var Account = require('../Account')
 
-var aa = new Account(true)
+var aa = new Account({
+  test: false,
+  profit: 62,
+  loss: -40,
+  shortProfit: 50
+})
 
-aa.orderLimit(6614, false, 2000).then((json) => {
+aa.orderLimit(7700, false, 2000).then((json) => {
   // console.log(json)
   // console.log(aa)
 })
