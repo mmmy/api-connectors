63fd457e0ab5168e337904ccdaa45a11907b63f4
yangqi <531905408@qq.com>
2018-08-01 00:12:55 +0800

    update port to 7777

--- official-ws/nodejs/strategy/researchStrategy/runSarMa.js
+++ official-ws/nodejs/strategy/researchStrategy/runSarMa.js
@@ -57,4 +57,4 @@ bitmex.listenCandle({binSize: '1h'}, function(list) {
 })
 
 
-const webserver = new WebServer({ port: 7005 }, obManager)
+const webserver = new WebServer({ port: 7777 }, obManager)
