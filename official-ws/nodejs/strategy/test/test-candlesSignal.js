03de04f4c4dd0a241a092d07e58617ea07c27b11
yangqi <531905408@qq.com>
2018-09-16 13:17:22 +0800

    update test psar

--- official-ws/nodejs/strategy/test/test-candlesSignal.js
+++ official-ws/nodejs/strategy/test/test-candlesSignal.js
@@ -3,13 +3,19 @@ var m5Data = require('../data/m5Data.json').reverse()
 const Candels = require('../Candles')
 
 var c = new Candels()
+c.setOptions({
+  sarStart: 0.017,
+  sarStep: 0.034,
+  sarMax: 0.13
+})
+
 var len = m5Data.length
 
 c.setHistoryData(m5Data)
 
 // test ok
-c.sarSignal()
+console.log(c.sarSignal())
 // test ok
-c.smaSignal()
+// c.smaSignal()
 
-c.sarSmaSignal()
+// c.sarSmaSignal()
