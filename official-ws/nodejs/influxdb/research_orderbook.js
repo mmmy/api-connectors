eb7ff2bae59bebbc38a032753aa1610bb429f459
杨旗 <yangq06@mysoft.com.cn>
2018-12-25 19:26:09 +0800

    u

--- official-ws/nodejs/influxdb/research_orderbook.js
+++ official-ws/nodejs/influxdb/research_orderbook.js
@@ -63,14 +63,15 @@ let _buyPrices = []
 let _sellBookCount = 0
 let _sellPrices = []
 
-const AFTER_SECONDS = 600 
+const AFTER_SECONDS = 150
+const LONG = false
 
-function recordAfterP(list, trades, seconds=60) {
+function recordAfterP(list, trades, seconds = 60) {
   if (trades.length === 0) {
     return
   }
   const d0 = trades[0]
-  for (let i=0; i<list.length; i++) {
+  for (let i = 0; i < list.length; i++) {
     const item = list[i]
     const timeAfter = new Date(d0.timestamp) - new Date(item.t)
     if (timeAfter < seconds * 1000 && timeAfter > 0) {
@@ -81,43 +82,51 @@ function recordAfterP(list, trades, seconds=60) {
 
 function tradeTest(json) {
   const { table, action, data } = json
-  _tradeCount ++
+  _tradeCount++
   let bid0 = ob.getTopBidPrice2(0)
   let ask0 = ob.getTopAskPrice2(0)
-  // let bid1 = ob.getTopBidPrice2(1E4)
+  let bid1 = ob.getTopBidPrice2(1E4)
   let ask1 = ob.getTopAskPrice2(1E4)
   // let bid2 = ob.getTopBidPrice2(1E5)
   // let ask2 = ob.getTopAskPrice2(1E5)
   let bid3 = ob.getTopBidPrice2(5E5)
-  // let ask3 = ob.getTopAskPrice2(1E6)
+  let ask3 = ob.getTopAskPrice2(5E5)
   const d0 = data[0]
-  if ((ask0 - bid0 === 0.5) && (bid0 - bid3) === 0 && (ask1 - ask0) > 1) {
-    const len = _buyPrices.length
-    const lastB = _buyPrices[len - 1]
-    if (len === 0 || (lastB.price !== bid0 && (new Date(d0.timestamp) - new Date(lastB.t)) > 30 * 1000)) {
-      _buyPrices.push({
-        p: bid0,
-        t: d0.timestamp,
-        afterPs: [],
-      })
+  if (LONG) {
+    if ((ask0 - bid0 === 0.5) && (bid0 - bid3) === 0 && (ask1 - ask0) > 1) {
+      const len = _buyPrices.length
+      const lastB = _buyPrices[len - 1]
+      if (len === 0 || (lastB.price !== bid0 && (new Date(d0.timestamp) - new Date(lastB.t)) > 30 * 1000)) {
+        _buyPrices.push({
+          side: 1,
+          p: bid0,
+          t: d0.timestamp,
+          afterPs: [],
+        })
+      }
+      // _buyPrices = _.uniqBy(_buyPrices, 'p')
+      _buyBookCount++
+      console.log('buy', _buyBookCount, _buyBookCount / _tradeCount, bid0, 'count', _buyPrices.length, d0.timestamp)
     }
-    // _buyPrices = _.uniqBy(_buyPrices, 'p')
-    _buyBookCount ++
-    console.log('buy', _buyBookCount, _buyBookCount / _tradeCount, bid0, 'count', _buyPrices.length, d0.timestamp)
+    recordAfterP(_buyPrices, data.filter(d => d.side === 'Sell'), AFTER_SECONDS)
   }
-  recordAfterP(_buyPrices, data.filter(d => d.side === 'Sell'), AFTER_SECONDS)
-  // if ((ask0 - bid0 === 0.5) && (ask0 - ask3) === 0 && (bid0 - bid1) > 1) {
-  //   const d0 = data[0]
-  //   _sellPrices.push(bid0)
-  //   _sellPrices = _.uniq(_sellPrices)
-  //   _sellBookCount ++
-  //   console.log('sell', _sellBookCount, _sellBookCount / _tradeCount, bid0, 'count', _sellPrices.length, d0.timestamp)
-  // }
-  // const times = data.map(item => item.timestamp)
-  // const timesUniq = _.uniq(times)
-  // if (times.length !== timesUniq.length) {
-  //   console.log('trade times are not uniq', times.length, timesUniq.length)
-  // }
+  if (!LONG) {
+    if ((ask0 - bid0 === 0.5) && (ask0 - ask3) === 0 && (bid0 - bid1) > 1) {
+      const len = _sellPrices.length
+      const lastS = _sellPrices[len - 1]
+      if (len === 0 || (lastS.price !== ask0 && (new Date(d0.timestamp) - new Date(lastS.t)) > 30 * 1000)) {
+        _sellPrices.push({
+          side: -1,
+          p: ask0,
+          t: d0.timestamp,
+          afterPs: [],
+        })
+      }
+      _sellBookCount++
+      console.log('sell', _sellBookCount, _sellBookCount / _tradeCount, ask0, 'count', _sellPrices.length, d0.timestamp)
+    }
+    recordAfterP(_sellPrices, data.filter(d => d.side === 'Buy'), AFTER_SECONDS)
+  }
   // 所有的timestamp都是同样值
   // const dataToInflux = data.map((item, i) => {
   //   if (i === 0) {
@@ -171,12 +180,36 @@ bitmex.listenOrderBook(orderBookTrade)
 bitmex.start()
 bitmex.on('end', () => {
   let wins = 0
-  const list = _buyPrices.map(item => {
+  const {
+    list,
+    sumHigh,
+    avgHigh,
+    timeSumHigh,
+    timeAvgHigh,
+    sumLow,
+    avgLow,
+    timeSumLow,
+    timeAvgLow
+  } = transform(LONG ? _buyPrices : _sellPrices)
+
+  console.log(list.slice(-1))
+  console.log(`${AFTER_SECONDS} long len`, list.length)
+  console.log('avgHigh', avgHigh, 'sumHigh', sumHigh, 'timeAvgHigh', timeAvgHigh)
+  console.log('avgLow', avgLow, 'sumLow', sumLow, 'timeAvgLow', timeAvgLow)
+
+  const filePath = `./temp/research_orderbook_${LONG ? 'LONG' : 'SHORT'}_${AFTER_SECONDS}.csv`
+  const csvStr = JSONtoCSV(list, ['t', 'p', 'diffHigh', 'timeHigh', 'diffLow', 'timeLow'])
+  fs.writeFileSync(path.join(__dirname, filePath), csvStr)
+  console.log('data end...')
+})
+
+function transform(positions) {
+  const list = positions.map(item => {
     const t = new Date(item.t)
     const afterPs = item.afterPs
     let maxPTrade = null
     let minPTrade = null
-    for (let i=0; i<afterPs.length; i++) {
+    for (let i = 0; i < afterPs.length; i++) {
       const tr = afterPs[i]
       if (!maxPTrade) {
         maxPTrade = tr
@@ -196,10 +229,6 @@ bitmex.on('end', () => {
 
     const diffHigh = maxPTrade.price - item.p
     const diffLow = minPTrade.price - item.p
-    const win = diffHigh === null ? null : (diffHigh > 0)
-    if (win) {
-      wins ++
-    }
     return {
       ...item,
       diffHigh,
@@ -217,14 +246,15 @@ bitmex.on('end', () => {
   const avgLow = sumLow / list.length
   const timeSumLow = list.reduce((s, item) => s + item.timeLow, 0)
   const timeAvgLow = timeSumLow / list.length
-
-  console.log(list.slice(-1))
-  console.log(`${AFTER_SECONDS} long len`, list.length)
-  console.log('avgHigh', avgHigh, 'sumHigh', sumHigh, 'timeAvgHigh', timeAvgHigh)
-  console.log('avgLow', avgLow, 'sumLow', sumLow, 'timeAvgLow', timeAvgLow)
-
-  const filePath = `./temp/research_orderbook_${AFTER_SECONDS}.csv`
-  const csvStr = JSONtoCSV(list, ['t','p','diffHigh', 'timeHigh', 'diffLow', 'timeLow'])
-  fs.writeFileSync(path.join(__dirname, filePath), csvStr)
-  console.log('data end...')
-})
+  return {
+    list,
+    sumHigh,
+    avgHigh,
+    timeSumHigh,
+    timeAvgHigh,
+    sumLow,
+    avgLow,
+    timeSumLow,
+    timeAvgLow
+  }
+}
