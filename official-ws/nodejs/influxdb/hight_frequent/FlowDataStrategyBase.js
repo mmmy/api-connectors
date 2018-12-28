cf001a736e7e9722cc0dc13a58e52aadf6225927
yangqi <531905408@qq.com>
2018-12-28 23:15:57 +0800

    re console

--- official-ws/nodejs/influxdb/hight_frequent/FlowDataStrategyBase.js
+++ official-ws/nodejs/influxdb/hight_frequent/FlowDataStrategyBase.js
@@ -509,8 +509,8 @@ class FlowDataStrategyBase {
 
   updateAccountOrder(json) {
     this._accountOrder.update(json)
-    console.log('-------------------------------- order')
-    console.log(json)
+    // console.log('-------------------------------- order')
+    // console.log(json)
     console.log(this._accountOrder)
   }
 
