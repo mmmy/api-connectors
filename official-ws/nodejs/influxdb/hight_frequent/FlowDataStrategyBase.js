41000b7d058d683f3a491cd916ac3bf733c2f95d
杨旗 <yangq06@mysoft.com.cn>
2018-12-29 15:58:24 +0800

    u

--- official-ws/nodejs/influxdb/hight_frequent/FlowDataStrategyBase.js
+++ official-ws/nodejs/influxdb/hight_frequent/FlowDataStrategyBase.js
@@ -509,7 +509,7 @@ class FlowDataStrategyBase {
 
   updateAccountOrder(json) {
     this._accountOrder.update(json)
-    console.log('account order update ordStatus', json.data[0] && json.data[0].ordStatus, 'ordType', json.data[0] && json.data[0].ordType)
+    console.log('account order update', json.action, '[ordStatus]', json.data[0] && json.data[0].ordStatus, '[ordType]', json.data[0] && json.data[0].ordType)
     // console.log(json)
     // console.log(this._accountOrder)
   }
