79ae339e4970f5a5f23b5e3eec316b5df7bdb22a
杨旗 <yangq06@mysoft.com.cn>
2019-01-28 21:10:48 +0800

    u

---
+++ official-ws/nodejs/influxdb/hight_frequent/humanStrategyServer/web/src/views/Trade/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import axios from 'axios'
+
+import './index.css'
+export default class Trade extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return 'mm'
+  }
+}
