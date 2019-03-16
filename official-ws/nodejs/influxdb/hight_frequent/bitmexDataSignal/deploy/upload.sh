dc87596b6e0c83d758af919846255f73d3d28e9a
yangqi <531905408@qq.com>
2019-03-16 11:57:27 +0800

    u

--- official-ws/nodejs/influxdb/hight_frequent/bitmexDataSignal/deploy/upload.sh
+++ official-ws/nodejs/influxdb/hight_frequent/bitmexDataSignal/deploy/upload.sh
@@ -1,4 +1,4 @@
 remote_ip="207.148.65.114"
 remote_path="/data/www/bitmex_data_signal"
 # sftp -b ../app/web/build/index.html -r root@$remote_ip:$remote_path
-scp -r ../web/build/* root@$remote_ip:$remote_path
+scp -r -P 22 ../web/build/* root@$remote_ip:$remote_path
