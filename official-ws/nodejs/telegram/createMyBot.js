e892d026c99f4aa4478861bca826db4ca34e191c
yangqi <531905408@qq.com>
2019-03-17 13:12:48 +0800

    u

--- official-ws/nodejs/telegram/createMyBot.js
+++ official-ws/nodejs/telegram/createMyBot.js
@@ -12,12 +12,42 @@ const bot = new Telegraf("753812904:AAFp6-Upva0MOo-T9QgiOc7Rs3qNOpJ7wRw", {
   }
 })
 
+// bot.on('text', (ctx) => {
+//   console.log(ctx.chat.id)
+//   ctx.reply('Hello World')
+// })
+
+let orderBookChartIds = []
+
+bot.command('orderbook_on', (ctx) => {
+  const id = ctx.chat.id
+  if (orderBookChartIds.indexOf(id) === -1) {
+    orderBookChartIds.push(id)
+    ctx.reply('成功订阅orderbook信号 💹')
+  } else {
+    ctx.reply('已经订阅过orderbook信号 🚼')
+  }
+})
+
+bot.command('orderbook_off', (ctx) => {
+  const id = ctx.chat.id 
+  const index = orderBookChartIds.indexOf(id)
+  if (index === -1) {
+    ctx.reply('未订阅过orderbook信号 👐')  
+  } else {
+    orderBookChartIds.splice(index, 1)
+    ctx.reply('成功退订orderbook信号 🌚')  
+  }
+})
+
 bot.launch()
 
 module.exports = {
   sendMessageToGroup: (msg) => {
-    bot.telegram.sendMessage("-320826333", msg).catch(e => {
-      console.log('send message to -320826333 error', e)
+    orderBookChartIds.forEach(id => {
+      bot.telegram.sendMessage(id, msg).catch(e => {
+        console.log(`send message to ${id} error`, e)
+      })
     })
   }
 }
