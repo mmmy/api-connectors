bfb44aa4f2570c3869c6acd665f12b787563cd75
杨旗 <yangq06@mysoft.com.cn>
2019-06-04 14:59:16 +0800

    update express log

--- official-ws/nodejs/influxdb/hight_frequent/humanStrategyServer/server/app.js
+++ official-ws/nodejs/influxdb/hight_frequent/humanStrategyServer/server/app.js
@@ -20,7 +20,9 @@ app.set('view engine', 'pug');
 app.use(Sentry.Handlers.requestHandler())
 app.use(Sentry.Handlers.errorHandler())
 
-app.use(logger('dev'));
+app.use(logger('dev', {
+  skip: function (req, res) { return res.statusCode < 400 }
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -31,12 +33,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api/coin', coinRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
