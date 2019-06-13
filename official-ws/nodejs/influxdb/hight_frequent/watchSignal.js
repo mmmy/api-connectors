c8fbae15ff82ffd5c2dd49449284e261f0d01c7e
杨旗 <yangq06@mysoft.com.cn>
2019-06-13 16:22:34 +0800

    u

--- official-ws/nodejs/influxdb/hight_frequent/watchSignal.js
+++ official-ws/nodejs/influxdb/hight_frequent/watchSignal.js
@@ -3,7 +3,7 @@ const notifyPhone = require('../../strategy/notifyPhone').notifyPhone
 function orderAction(strategy, autoOrder) {
   const { symbol, side, amount, order_method, signal_name, signal_operator } = autoOrder
   const sdk = strategy._orderManager.getSignatureSDK()
-  switch(order_method) {
+  switch (order_method) {
     case 'stopMarket1h':
       strategy.orderStopOrderByLastCandle(symbol, '1h', amount, side).catch(e => {
         setTimeout(() => { strategy.orderStopOrderByLastCandle(symbol, '1h', amount, side) }, 10 * 1000)
@@ -31,13 +31,13 @@ function orderAction(strategy, autoOrder) {
 
 function watchSignal(strategy, symbol, signal_name, signal_operator, signal_value) {
   const autoOrderSignals = strategy.getAutoOrderSignalList()
-  const signalNameAutoOrdes = autoOrderSignals.filter(a => 
+  const signalNameAutoOrdes = autoOrderSignals.filter(a =>
     a.symbol === symbol
     && a.signal_name === signal_name
     && a.signal_operator === signal_operator
     && a.remain_times > 0
   )
-  
+
   if (signalNameAutoOrdes.length === 0) {
     return
   }
@@ -46,11 +46,11 @@ function watchSignal(strategy, symbol, signal_name, signal_operator, signal_valu
     const now = new Date()
     const { last_exec_time, min_interval } = autoOrder
     // 小于最小时间间隔不执行 （小时）
-    if (last_exec_time && (now - new Date(last_exec_time) < min_interval * 3600 * 1000 )) {
+    if (last_exec_time && (now - new Date(last_exec_time) < min_interval * 3600 * 1000)) {
       return
     }
     // 如果是highlow, 先判断次数
-    if (autoOrder.signal_name === 'high1' || autoOrder.signal_name === 'low1') {
+    if (autoOrder.signal_name === 'break1h' || autoOrder.signal_name === 'break5m') {
       const values = autoOrder.values
       if (values.times > 1) {
         values.times = values.times - 1
