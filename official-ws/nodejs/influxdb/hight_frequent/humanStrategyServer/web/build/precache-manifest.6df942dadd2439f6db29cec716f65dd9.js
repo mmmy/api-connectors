bad6e085bb882d1a5af97c4b8ddbb4a3772465fb
杨旗 <yangq06@mysoft.com.cn>
2019-06-15 18:51:14 +0800

    u

--- official-ws/nodejs/influxdb/hight_frequent/humanStrategyServer/web/build/precache-manifest.6df942dadd2439f6db29cec716f65dd9.js
+++ official-ws/nodejs/influxdb/hight_frequent/humanStrategyServer/web/build/precache-manifest.6df942dadd2439f6db29cec716f65dd9.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "84ee51307e4055a12559",
-    "url": "/static/js/main.84ee5130.chunk.js"
+    "revision": "37608ab9826467370e61",
+    "url": "/static/js/main.37608ab9.chunk.js"
   },
   {
     "revision": "5ef3a5d02f04ef20c813",
     "url": "/static/js/1.5ef3a5d0.chunk.js"
   },
   {
-    "revision": "84ee51307e4055a12559",
+    "revision": "37608ab9826467370e61",
     "url": "/static/css/main.00f63997.chunk.css"
   },
   {
-    "revision": "20c58636d1cf05ab858c15da5e259886",
+    "revision": "0f5a99d639ea4c667f15580b3d540624",
     "url": "/index.html"
   }
 ];
--- official-ws/nodejs/influxdb/hight_frequent/humanStrategyServer/web/build/service-worker.js
+++ official-ws/nodejs/influxdb/hight_frequent/humanStrategyServer/web/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a43bb6d1427c7876f698b20c976328a3.js"
+  "/precache-manifest.6df942dadd2439f6db29cec716f65dd9.js"
 );
 
 workbox.clientsClaim();
