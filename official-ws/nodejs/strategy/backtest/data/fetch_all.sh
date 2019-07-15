cff003bab54c21d9aac69a75cc50f4c96c0a8231
杨旗 <yangq06@mysoft.com.cn>
2019-07-15 19:36:46 +0800

    u

--- official-ws/nodejs/strategy/backtest/data/fetch_all.sh
+++ official-ws/nodejs/strategy/backtest/data/fetch_all.sh
@@ -1,3 +1,9 @@
+# symbol="XBTUSD"
+# if [ -n "$1" ]
+# then
+#   symbol=$1
+# fi;
+# echo $symbol
 node xbt_fetch_data.js -s XBTUSD -b 1d
 node check_data.js XBTUSD_1d_2017-04-01.csv
 
@@ -9,3 +15,15 @@ node check_data.js XBTUSD_5m_2017-09-01.csv
 
 node xbt_fetch_data.js -s XBTUSD -b 1m
 node check_data.js XBTUSD_1m_2017-09-01.csv
+
+node xbt_fetch_data.js -s ETHUSD -b 1d
+node check_data.js ETHUSD_1d_2018-08-03.csv
+
+node xbt_fetch_data.js -s ETHUSD -b 1h
+node check_data.js ETHUSD_1h_2018-08-03.csv
+
+node xbt_fetch_data.js -s ETHUSD -b 5m
+node check_data.js ETHUSD_5m_2018-08-03.csv
+
+node xbt_fetch_data.js -s ETHUSD -b 1m
+node check_data.js ETHUSD_1m_2018-08-03.csv
